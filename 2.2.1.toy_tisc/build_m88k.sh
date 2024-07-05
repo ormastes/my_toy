82b0081485dd82fba22e5cddd732fb0e6e51a192
Yoon, Jonghyun <ormastes@gmail.com>
2024-07-05 08:32:05 +0000

    build script updated

--- 2.2.1.toy_tisc/build_m88k.sh
+++ 2.2.1.toy_tisc/build_m88k.sh
@@ -1,15 +1,14 @@
 cd ../2.0.0.llvm
 cd llvm-project
-git switch toy_msp430_new_command
+git switch toy_tisc
 cd ..
 mkdir build
 cd build
 # ;libcxx;libcxxabi
 #  -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" 
 # -DCMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" 
-
 export CC=/usr/bin/clang-18
 export CXX=/usr/bin/clang++-18
 # -DLLVM_LLD_ENABLE=ON -DCMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" >> changed to >> -DLLVM_USE_LINKER=lld
-cmake -G Ninja -DLLVM_USE_LINKER=lld -DLLVM_DEFAULT_TARGET_TRIPLE=toymsp43_-unknown-none -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/home/ormastes/dev/install/clang_17_X86_reldeb/lib/cmake/llvm -DLLVM_TARGETS_TO_BUILD="TOYMSP43_" -DLLVM_ENABLE_PROJECTS="clang" ../llvm-project/llvm 
+cmake -G Ninja -DLLVM_USE_LINKER=lld -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=M88k -DLLVM_DEFAULT_TARGET_TRIPLE=m88k-unknown-none -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/home/ormastes/dev/install/clang_18_m88k_debug/lib/cmake/llvm -DLLVM_TARGETS_TO_BUILD="M88k" -DLLVM_ENABLE_PROJECTS="clang" ../llvm-project/llvm 
 ninja
--- 2.2.2.toy_tisc_command/build.sh
+++ 2.2.2.toy_tisc_command/build.sh
@@ -8,5 +8,8 @@ cd build
 #  -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" 
 # -DCMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" 
 
-cmake -G Ninja -DLLVM_LLD_ENABLE=ON -DLLVM_DEFAULT_TARGET_TRIPLE=toymsp43_-unknown-none -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/home/ormastes/dev/install/clang_17_X86_reldeb/lib/cmake/llvm -DLLVM_TARGETS_TO_BUILD="TOYMSP43_" -DLLVM_ENABLE_PROJECTS="clang" ../llvm-project/llvm 
+export CC=/usr/bin/clang-18
+export CXX=/usr/bin/clang++-18
+# -DLLVM_LLD_ENABLE=ON -DCMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" >> changed to >> -DLLVM_USE_LINKER=lld
+cmake -G Ninja -DLLVM_USE_LINKER=lld -DLLVM_DEFAULT_TARGET_TRIPLE=tisc-unknown-none -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/home/ormastes/dev/install/clang_18_tisc_debug/lib/cmake/llvm -DLLVM_TARGETS_TO_BUILD="TISC" -DLLVM_ENABLE_PROJECTS="clang" ../llvm-project/llvm 
 ninja
